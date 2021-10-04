04881525ede4a2dc0a333fa5e1518da794152a9f
alexanderkell <alexander@kell.es>
2021-10-04 18:24:40 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Find robust bounds for a flexible power systems',
+    author='Alexander J. M. Kell',
+    license='MIT',
+)
